9ea520637fdd5befb1ba48ca7c0bfc3045548793
siwon9 <siwon10823@gmail.com>
2024-05-15 21:42:45 +0900

    Collection 복습

---
+++ 2. 서블릿 & JSP/exam02/src/main/java/exam02/StackQueue.java
@@ -0,0 +1,13 @@
+package exam02;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackQueue {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        Queue q = new LinkedList();
+    }
+}
