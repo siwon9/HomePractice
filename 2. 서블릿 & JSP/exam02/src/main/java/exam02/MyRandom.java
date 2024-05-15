7ac8d5a1239baeaffdc7e9ad9d2a8dc8e52952f9
siwon9 <siwon10823@gmail.com>
2024-05-15 14:31:47 +0900

    RANDOM 사요

---
+++ 2. 서블릿 & JSP/exam02/src/main/java/exam02/MyRandom.java
@@ -0,0 +1,11 @@
+package exam02;
+
+import java.util.Random;
+
+public class MyRandom {
+        public static void main(String[] args) {
+            
+            int num = new Random().nextInt(6) + 1;
+        }
+
+}
